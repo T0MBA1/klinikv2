9956765ae25fa7db578464bf74220c8e0f7e9025
Arzed <tosoon149@gmail.com>
2023-09-25 14:03:35 +0700

    edit-login-page

--- src/components/LoginOne.jsx
+++ src/components/LoginOne.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import { Col, Container, Row } from 'reactstrap';
-import Image from './ui/Media';
 import LoginForm from './LoginForm';
+import Image from './ui/Media';
 
 const LoginOne = () => {
   return (
@@ -9,10 +9,10 @@ const LoginOne = () => {
       <section>
         <Container fluid={true}>
           <Row>
-            <Col xl='5' className='b-center bg-size' style={{ backgroundImage: `url(${require('../assets/images/2.jpg')})`, backgroundSize: 'cover', backgroundPosition: 'center', display: 'block' }}>
+            <Col xl='7' className='b-center bg-size' style={{ backgroundImage: `url(${require('../assets/images/2.jpg')})`, backgroundSize: 'cover', backgroundPosition: 'center', display: 'block' }}>
               <Image attrImage={{ className: 'bg-img-cover bg-center d-none', src: `${require('../assets/images/2.jpg')}`, alt: 'looginpage' }} />
             </Col>
-            <Col xl='7 p-0'>
+            <Col xl='5 p-0'>
               <LoginForm logoClassMain='text-start' />
             </Col>
           </Row>
